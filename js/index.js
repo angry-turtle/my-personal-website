d7435368bdf941c643250f53d9384d6a0f93fd0e
Volodya Kalash <brooklynbaby@ukr.net>
2018-12-03 16:57:40 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
-const clock = document.getElementById('clock');
-setInterval(() => clock.innerText = (new Date()).toLocaleTimeString(),1000);
-
-
-
+$(function() {
+    $('.smooth').on('click', function(event) {
+        const target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
